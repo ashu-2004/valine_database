1948cb9fa6f5da3415b84d3e10701c6af3091973
ashuthikole2004 <ashwinithikole@gmail.com>
2025-05-08 13:22:10 +0530

    tenth commit

--- config/db.js
+++ config/db.js
@@ -23,27 +23,33 @@
 
 // module.exports = db;
 
-const { Sequelize } = require("sequelize");
-require("dotenv").config(); // Load environment variables from .env file
-console.log("Database url",process.env.DATABASE_URL);
-const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "mysql",
-    logging: false,
-    dialectOptions: {
-        ssl: false, // Railway MySQL typically doesn't use SSL
-    },
-});
+    const { Sequelize } = require("sequelize");
+    require("dotenv").config(); // Load environment variables from .env file
+    console.log("Database name",process.env.DB_NAME);
+    console.log("Database user",process.env.DB_USER);
+    console.log("Database password",process.env.DB_PASS);
+    console.log("Database host",process.env.DB_HOST);
+    console.log("Database port",process.env.DB_PORT);
+    console.log("Database env",process.env);
+    console.log("Database url",process.env.DATABASE_URL);
+    const db = new Sequelize(process.env.DATABASE_URL, {
+        dialect: "mysql",
+        logging: false,
+        dialectOptions: {
+            ssl: false, // Railway MySQL typically doesn't use SSL
+        },
+    });
 
-const connection = async () => {
-    try {
-        await db.authenticate();
-        console.log("✅ Database connected successfully.");
-    } catch (err) {
-        console.error("❌ Unable to connect to the database:", err);
-    }
-};
+    const connection = async () => {
+        try {
+            await db.authenticate();
+            console.log("✅ Database connected successfully.");
+        } catch (err) {
+            console.error("❌ Unable to connect to the database:", err);
+        }
+    };
 
-// Call connection function
-connection();
+    // Call connection function
+    connection();
 
-module.exports = db;
+    module.exports = db;
